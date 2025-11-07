38fd79352d6bf42f27470cfc7487e015343b7d9c
sayandh <sayandh871@gmail.com>
2025-11-07 11:47:25 +0530

    build time issues fixed

--- src/app/pro/page.tsx
+++ src/app/pro/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Navbar from "@/components/Navbar";
 import { PricingTable } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
